ec03c2b96356a67250ced0481418f773f1d401fd
Norwak <norwak99@gmail.com>
2024-01-10 13:06:24 +0500

    Created a unit test for AuthService, checking if it's defined

--- 7-car-price-estimate/src/users/auth.service.spec.ts
+++ 7-car-price-estimate/src/users/auth.service.spec.ts
@@ -1,18 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+    // Create a fake copy of the users service
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User),
+    };
+  
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+  
     }).compile();
-
-    service = module.get<AuthService>(AuthService);
+  
+    service = module.get(AuthService);
   });
-
-  it('should be defined', () => {
+  
+  it('can create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
 });
