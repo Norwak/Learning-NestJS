c7c03e3af4fcebd4147e17802635c9b22f272b5a
Norwak <norwak99@gmail.com>
2024-01-12 11:54:48 +0500

    Added POST route to reports

--- 7-car-price-estimate/src/reports/reports.service.ts
+++ 7-car-price-estimate/src/reports/reports.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Report } from './entities/report.entity';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(
+    @InjectRepository(Report) private reportRepository: Repository<Report>,
+  ) {}
+
+  async create(createReportDto: CreateReportDto) {
+    const report = this.reportRepository.create(createReportDto);
+    return this.reportRepository.save(report);
+  }
+}
