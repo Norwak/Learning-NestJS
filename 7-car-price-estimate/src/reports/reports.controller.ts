b2cd035bedea630f681e375fed6251e4c3bb5cbc
Norwak <norwak99@gmail.com>
2024-01-12 15:07:30 +0500

    Added relation one-to-many between users and reports

--- 7-car-price-estimate/src/reports/reports.controller.ts
+++ 7-car-price-estimate/src/reports/reports.controller.ts
@@ -2,8 +2,13 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/entities/user.entity';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { ReportDto } from './dtos/report.dto';
 
 @Controller('reports')
+@Serialize(ReportDto)
 export class ReportsController {
   constructor(
     private reportsService: ReportsService
@@ -11,7 +16,7 @@ export class ReportsController {
     
   @Post()
   @UseGuards(AuthGuard)
-  async createReport(@Body() createReportDto: CreateReportDto) {
-    return await this.reportsService.create(createReportDto);
+  async createReport(@CurrentUser() user: User, @Body() createReportDto: CreateReportDto) {
+    return await this.reportsService.create(user, createReportDto);
   }
 }
--- 7-car-price-estimate/src/reports/reports.service.ts
+++ 7-car-price-estimate/src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './entities/report.entity';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
 export class ReportsService {
@@ -10,8 +11,9 @@ export class ReportsService {
     @InjectRepository(Report) private reportRepository: Repository<Report>,
   ) {}
 
-  async create(createReportDto: CreateReportDto) {
+  async create(user: User, createReportDto: CreateReportDto) {
     const report = this.reportRepository.create(createReportDto);
+    report.user = user;
     return this.reportRepository.save(report);
   }
 }
