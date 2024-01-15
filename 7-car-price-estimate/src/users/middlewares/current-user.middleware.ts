e786e17500d5ad111753202647d2538ad9b882e4
Norwak <norwak99@gmail.com>
2024-01-15 11:39:12 +0500

    Fixed admin guard by changing currentUser interceptor into middleware

---
+++ 7-car-price-estimate/src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,30 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { UsersService } from "../users.service";
+import { User } from "../entities/user.entity";
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(
+    private usersService: UsersService,
+  ) {}
+
+  async use(request: Request, response: Response, next: NextFunction) {
+    const {userId} = request.session || {};
+
+    if (userId) {
+      const user = await this.usersService.findOne(userId);
+      request.currentUser = user;
+    }
+
+    next();
+  }
+}
